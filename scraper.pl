c18e610f3901c375e32f3c6ee7b23732164f1242
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-08 15:38:38 +0000

    lang/perl/Moxy: auto detect library path.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@2905 d0d07461-0603-4401-acd4-de1884942a52

--- scraper.pl
+++ scraper.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use FindBin;
+use File::Spec;
+use lib File::Spec->catfile($FindBin::Bin, 'lib');
 
 use Moxy::Scraper;
 my @carrier = @ARGV ? @ARGV : qw/ i e v /;
