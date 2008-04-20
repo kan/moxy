2f7bf3ac0b8d6ce2451b273236c038b994ce2bf3
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-20 04:55:31 +0000

    config でどのライブラリつかうか選べるようにした。
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@9974 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Moxy/Plugin/QRCode.pm
+++ lib/Moxy/Plugin/QRCode.pm
@@ -2,8 +2,6 @@ package Moxy::Plugin::QRCode;
 use strict;
 use warnings;
 use base qw/Moxy::Plugin/;
-use GD::Barcode;
-use GD::Barcode::QRcode;
 use URI::Escape;
 
 sub control_panel:Hook('control_panel') {
@@ -23,15 +21,37 @@ sub request_filter:Hook('request_filter') {
     if ($args->{request}->uri =~ m{^http://qrcode\.moxy/(.+)}) {
         my $url = uri_unescape($1);
 
-
-        my $qrcode = GD::Barcode::QRcode->new( $url,
-            { Ecc => 'M', ModuleSize => 5, Version => 5 } )
-            ->plot->png;
+        my $engine = $self->config->{config}->{engine} || 'GD';
+        my $qrcode = _generate_qr($url, $engine);
 
         my $response = HTTP::Response->new( 200, 'Moxy QRcode ok' );
         $response->header('Content-Type' => 'image/png');
         $response->content($qrcode);
         $response;
+    }
+}
+
+sub _generate_qr {
+    my ($url, $engine) = @_;
+
+    if ($engine =~ /Imager/i) {
+        require Imager::QRCode;
+        my $qrcode = Imager::QRCode->new(
+            level => 'M',
+            casesensitive => 1,
+        );
+
+        my $image = $qrcode->plot($url);
+        $image->write( data => \my $data, type => 'png' );
+        return $data;
+    } elsif ($engine =~ /^GD$/i) {
+        require GD::Barcode;
+        require GD::Barcode::QRcode;
+        return GD::Barcode::QRcode->new( $url,
+            { Ecc => 'M', ModuleSize => 5, Version => 5 } )
+            ->plot->png;
+    } else {
+        die "unknown qrcode engine type: $engine";
     }
 }
 
@@ -45,6 +65,10 @@ Moxy::Plugin::QRCode - QRCode generator for Moxy
 =head1 SYNOPSIS
 
     - module: QRCode
+      engine: Imager
+
+    - module: QRCode
+      engine: GD
 
 =head1 DESCRIPTION
 
@@ -53,6 +77,14 @@ QRCode generator for Moxy.
 =head1 DEPENDENCY
 
 This module uses L<GD::Barcode::QRcode>.
+
+=head1 AUTHORS
+
+Kan Fushihara
+
+Tokuhiro Matsuno
+
+Daisuke Murase
 
 =head1 SEE ALSO
 
