0c927faf259bacbfdc0fb1ef92ff5a354b746b6e
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-22 02:18:16 +0000

    途中にコメントいれるのキライ！
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@10124 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Moxy/Catalyst.pm
+++ lib/Moxy/Catalyst.pm
@@ -1,36 +1,16 @@
 package Moxy::Catalyst;
-
 use strict;
 use warnings;
-
 use Catalyst::Runtime '5.70';
-
-# Set flags and add plugins for the application
-#
-#         -Debug: activates the debug mode for very useful log messages
-#   ConfigLoader: will load the configuration from a YAML file in the
-#                 application's home directory
-# Static::Simple: will serve static files from the application's root 
-#                 directory
-
 use Catalyst qw/-Debug ConfigLoader Static::Simple/;
-
-our $VERSION = '0.01';
-
-# Configure the application. 
-#
-# Note that settings in moxy_catalyst.yml (or other external
-# configuration file that you set up manually) take precedence
-# over this when using ConfigLoader. Thus configuration
-# details given here can function as a default configuration,
-# with a external configuration file acting as an override for
-# local deployment.
 
 __PACKAGE__->config( name => 'Moxy::Catalyst' );
 
-# Start the application
 __PACKAGE__->setup;
 
+
+1;
+__END__
 
 =head1 NAME
 
@@ -58,5 +38,3 @@ This library is free software, you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
-
-1;
