83c5519ae2961fdbb72bf9346fa7ac1b9ab90925
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-28 11:18:21 +0000

    absoluteURI じゃなければ absoluteURI にする
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@10673 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Moxy/Plugin/RelativeLocation.pm
@@ -0,0 +1,27 @@
+package Moxy::Plugin::RelativeLocation;
+use strict;
+use warnings;
+use base qw/Moxy::Plugin/;
+
+use URI;
+
+sub response_filter :Hook {
+    my ($self, $context, $args) = @_;
+
+    my $location = $args->{response}->header('Location');
+
+    unless ($location =~ m!^https?://!) {
+        my $base = $args->{response}->request->uri;
+        my $url = sprintf '%s://%s', $base->scheme, $base->host;
+        unless (($base->scheme eq 'http' && $base->port eq '80') ||
+               ($base->scheme eq 'https' && $base->port eq '443')) {
+            $url .= ':' . $base->port;
+        }
+        $url .= $base->path;
+        $location = URI->new_abs($location, $url);
+    }
+    $args->{response}->header( Location => $location );
+}
+
+
+1;
