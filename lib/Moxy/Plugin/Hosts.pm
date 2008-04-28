48cf360b6f934dc2d0e894deb53778577cc3d32b
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-28 10:21:39 +0000

    /etc/hosts ファイルみたいに特定のhostを違うip addressに飛ばすしくみ
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@10663 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Moxy/Plugin/Hosts.pm
@@ -0,0 +1,40 @@
+package Moxy::Plugin::Hosts;
+use strict;
+use warnings;
+use base qw/Moxy::Plugin/;
+
+sub request_filter :Hook('request_filter') {
+    my ($self, $context, $args) = @_;
+
+    my $hosts = $self->config->{config}->{hosts};
+    my $uri   = $args->{request}->uri;
+    if (my $ip = $hosts->{$uri->host}) {
+        my $host = $uri->host;
+        $context->log( debug => " $host -> $ip " );
+        $uri->host($ip);
+        $args->{request}->uri($uri);
+
+        $args->{request}->header( Host => $host );
+    }
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Moxy::Plugin::Hosts - /etc/hosts file emulator
+
+=head1 SYNOPSIS
+    - module: Hosts
+      config:
+        hosts:
+          coderepos.org: 192.168.0.1
+
+=head1 AUTHOR
+
+    Kazuhiro Osawa
+
+=head1 SEE ALSO
+
+L<Moxy>
