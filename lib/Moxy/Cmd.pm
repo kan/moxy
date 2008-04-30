90ac583c6f9f34cd561aefdc66d2a74401e2b0fb
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-04-30 05:33:39 +0000

    use App::Cmd.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@10789 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/Moxy/Cmd.pm
@@ -0,0 +1,78 @@
+package Moxy::Cmd;
+use strict;
+use warnings;
+use base qw/App::Cmd::Simple/;
+
+use File::Spec::Functions;
+use FindBin;
+use HTTP::Engine;
+use Moxy;
+use YAML;
+
+sub opt_spec {
+    return (
+        [ 'daemonize|d' => "daemonize" ],
+        [
+            'config|c' => "path to configuration file",
+            { default => catfile( $FindBin::Bin, 'config.yaml' ) }
+        ],
+        [ 'help|h' => "display manual" ]
+    );
+}
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+    die $self->usage() if $opt->{help};
+}
+
+sub run {
+    my ($self, $opt, $args) = @_;
+
+    my $config = YAML::LoadFile( $opt->{config} );
+    $config->{global}->{log} ||= { level => 'debug' };
+
+    if ($opt->{daemonize}) {
+        if (my $pid = fork) {
+            exit 0; 
+        } elsif (defined $pid) {
+            $self->_stdio_close();
+            $self->_start($config);
+        } else {
+            die "fork failed: $@";
+        }
+    } else {
+        $self->_start($config);
+    }
+}
+
+sub _start {
+    my ($self, $config) = @_;
+
+    my $moxy = Moxy->new($config);
+    my $server_conf = {
+        global => $config->{global},
+        plugins => [
+            $config->{global}->{server},
+            {module => 'DebugScreen', }
+        ]
+    };
+    HTTP::Engine->new(
+        config         => $server_conf,
+        handle_request => sub {
+            my $c = shift;
+            $moxy->handle_request( $c );
+        },
+    )->run;
+}
+
+sub _stdio_close {
+    close(STDIN);
+    close(STDOUT);
+    close(STDERR);
+
+    open(STDIN,  "+>/dev/null"); ## no critic.
+    open(STDOUT, "+>&STDIN");    ## no critic.
+    open(STDERR, "+>&STDIN");    ## no critic.
+}
+
+1;
--- moxy.pl
+++ moxy.pl
@@ -2,81 +2,8 @@
 use strict;
 use warnings;
 use FindBin;
-use Path::Class;
-use YAML;
-use Getopt::Long;
-use Pod::Usage;
-use HTTP::Engine;
+use File::Spec::Functions;
+use lib catfile( $FindBin::Bin, 'lib' );
+use Moxy::Cmd;
 
-use lib file( $FindBin::RealBin, 'lib' )->stringify;
-use Moxy;
-
-sub stdio_close {
-    close(STDIN);
-    close(STDOUT);
-    close(STDERR);
-
-    open(STDIN,  "+>/dev/null"); ## no critic.
-    open(STDOUT, "+>&STDIN");    ## no critic.
-    open(STDERR, "+>&STDIN");    ## no critic.
-}
-
-my $conf_file = file( $FindBin::RealBin, 'config.yaml' )->stringify;
-
-Getopt::Long::GetOptions(
-    '--man'           => \my $man,
-    '--daemon'        => \my $daemon,
-    '--config=s'      => \$conf_file,
-) or pod2usage(2);
-Getopt::Long::Configure("bundling");
-pod2usage(-verbose => 2) if $man;
-
-my $config = YAML::LoadFile($conf_file);
-$config->{global}->{log} ||= { level => 'debug' };
-
-sub start {
-    my $moxy = Moxy->new($config);
-    my $server_conf = {
-        global => $config->{global},
-        plugins => [
-            $config->{global}->{server},
-            {module => 'DebugScreen', }
-        ]
-    };
-    HTTP::Engine->new(
-        config         => $server_conf,
-        handle_request => sub {
-            my $c = shift;
-            $moxy->handle_request( $c );
-        },
-    )->run;
-}
-
-if ($daemon) {
-    if (my $pid = fork) {
-        exit 0; 
-    } elsif (defined $pid) {
-        stdio_close();
-        start();
-    } else {
-        die "fork failed: $@";
-    }
-} else {
-    start();
-}
-
-__END__
-
-=head1 SYNOPSIS
-
-    $ moxy.pl
-
-    Options:
-        --daemon       => run as daemon
-        --config=s     => path to config file(default: config.yaml)
-        --man          => show this manual
-
-=head1 DESCRIPTION
-
-Proxy server for mobile web service development.
-
+Moxy::Cmd->run;
