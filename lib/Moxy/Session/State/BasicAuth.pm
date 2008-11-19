dd5c51404002a4f9a7a9795b383837313ad0464f
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-19 02:55:30 +0000

    HTTP::Session 0.20 対応
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Moxy/trunk@24221 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Moxy/Session/State/BasicAuth.pm
+++ lib/Moxy/Session/State/BasicAuth.pm
@@ -1,14 +1,11 @@
 package Moxy::Session::State::BasicAuth;
-use Moose;
-with 'HTTP::Session::Role::State';
+use HTTP::Session::State::Base;
 
-has realm => (
-    is  => 'rw',
-    isa => 'Str',
-    default => 'Moxy needs basic auth.Only for identification.Password is dummy.',
-);
+sub realm { 'Moxy needs basic auth.Only for identification.Password is dummy.' }
 
-has '+permissive' => ( 'default' => 1 );
+sub new {
+    bless {permissive => 1}, shift;
+}
 
 sub get_session_id {
     my ( $self, $req ) = @_;
@@ -17,7 +14,5 @@ sub get_session_id {
 
 sub response_filter { }
 
-no Moose;
-__PACKAGE__->meta->make_immutable;
 1;
 
