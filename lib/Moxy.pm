fd06651ec4bc990165d1f93962f620e6c74e2060
Masayuki Matsuki <y.songmu@gmail.com>
2012-03-18 13:56:16 +0900

    OpenSocialプラグインはデフォルトでは読み込まないように

--- lib/Moxy.pm
+++ lib/Moxy.pm
@@ -51,7 +51,7 @@ __PACKAGE__->load_components(qw/Plaggerize Autocall::InjectMethod Context/);
 
 __PACKAGE__->load_plugins(qw/
     DisplayWidth ControlPanel LocationBar Pictogram
-    Status::401 Status::500 Status::404 OpenSocial
+    Status::401 Status::500 Status::404
     UserID XMLisHTML UserAgentSwitcher RefererCutter CookieCutter FlashUseImgTag
     DisableTableTag GPS HTTPHeader QRCode ShowHTTPHeaders
 /);
